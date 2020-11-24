b3a60c165b6815b41147fc7c515d2f8a78c82bd9
Jakub Kleban <jakub.kleban2000@gmail.com>
2020-11-24 01:13:30 +0100

    Add Bootstrap CSS & simplify code

--- index.php
+++ index.php
@@ -25,36 +25,41 @@
 require_once(__DIR__ . '/../../config.php');
 
 require_login();
+
 $username = optional_param('username', null, PARAM_ALPHA);
-$worldname = get_string('world', 'local_helloworld');
+$validusername = isset($username) && !empty($username);
+if ($validusername) {
+    $heading = get_string('hellouser', 'local_helloworld', $username);
+} else {
+    $worldname = get_string('world', 'local_helloworld');
+    $heading = get_string('hellouser', 'local_helloworld', $worldname);
+}
+
+$PAGE->set_url(new moodle_url('/local/helloworld/index.php'));
 $PAGE->set_context(context_system::instance());
 $PAGE->set_title(get_string('pluginname', 'local_helloworld'));
-$PAGE->set_url(new moodle_url('/local/helloworld/index.php'));
+$PAGE->set_heading($heading);
 $PAGE->set_pagelayout('standard');
-
-if (isset($username) && !empty($username)){
-    $PAGE->set_heading(get_string('hellouser', 'local_helloworld', $username));
-} else {
-    $PAGE->set_heading(get_string('hellouser', 'local_helloworld', $worldname));
-}
 
 echo $OUTPUT->header();
 
-if (isset($username) && !empty($username)) {
-    echo "<h1>". get_string('hellouser', 'local_helloworld', $username) ."</h1>";
-    $frontpageurl = new moodle_url('/');
+echo '<div>';
+if ($validusername) {
     echo '<ul>';
-    echo '<li><a href="'.$frontpageurl.'">' . get_string('backtohome', 'core_moodle') . '</a></li>';
-    echo '<li><a href="'.$PAGE->url.'">' . get_string('backtopageyouwereon', 'core_moodle') . '</a></li>';
+    echo '<li><a href="' . new moodle_url('/') . '">' . get_string('backtohome', 'core_moodle') . '</a></li>';
+    echo '<li><a href="' . $PAGE->url . '">' . get_string('backtopageyouwereon', 'core_moodle') . '</a></li>';
     echo '</ul>';
 } else {
-    echo '<h1>' . get_string('hellouser', 'local_helloworld', $worldname) . '</h1>';
-    echo '<p>' . get_string('questionname', 'local_helloworld') . '</p>';
-    echo '<form method="get" action="'.$PAGE->url.'">';
     $promptname = get_string('promptname', 'local_helloworld');
-    echo '   <input type="text" name="username" placeholder="' . s($promptname) . '">';
-    echo '   <input type="submit" value="' . get_string('submit', 'core_moodle') . '">';
+
+    echo '<p>' . get_string('questionname', 'local_helloworld') . '</p>';
+    echo '<form class="form-inline" method="get" action="'.$PAGE->url.'">';
+    echo '  <div class="form-group">';
+    echo '      <input type="text" class="form-control mx-sm-3" name="username" placeholder="' . s($promptname) . '">';
+    echo '      <input type="submit" class="btn btn-primary" value="' . get_string('submit', 'core_moodle') . '">';
+    echo '  </div>';
     echo '</form>';
 }
+echo '</div>';
 
 echo $OUTPUT->footer();
